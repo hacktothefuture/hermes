62c670fb6a6a579ca42006d520040126d5fe2659
Mitchell Sternke <mitchellsternke@gmail.com>
2015-04-25 12:15:16 -0500

    Added prototypes of HTTP methods

--- server/server.js
+++ server/server.js
@@ -1,15 +1,11 @@
 var days_to_expiration = 7;
 var http = require("http");
+var express = require('express');
 
-http.createServer( function(request, response) {
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("Hello, World!");
-	response.end();
-}).listen(8888);
+var app = express();
 
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
-var ObjectId = require('mongodb').ObjectID;
 var url = 'mongodb://localhost:27017/test';
 
 MongoClient.connect(url, function(err, db) {
@@ -25,13 +21,13 @@ var insertBoard = function(db, message_info, callback) {
 	var point = {"type": "Point",
 					"coordinates": [location.lat, location.lon]};
 	db.collection('board').insertOne( {
-		"location" : point;
-		"board": [message_info.content];
+		"location" : point,
+		"board": [message_info.content]
 	});
 };
 
 
-var findBoards = function(db, request_info callback) {
+var findBoards = function(db, request_info, callback) {
 	var location = message_info.location;
 	var point = {"type": "Point",
 					"coordinates": [location.lat, location.lon]};
@@ -55,3 +51,51 @@ var findBoards = function(db, request_info callback) {
 // 		db.close();
 // 	});
 // });
+
+/*
+http.createServer( function(request, response) {
+	var pathname = require('url').parse(request.url).pathname;
+	if( pathname == '/get_messages' && request.method == 'GET' ) {
+	} else if ( pathname == '/send_message' && request.method == 'POST' ) {
+	}
+	
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	response.write("Hello, World!");
+	response.end();
+}).listen(8888);
+*/
+
+// GET: /hello
+app.get( '/hello', function(request, response){
+	response.send('Hello, World!');
+});
+
+// GET: /get_messages
+app.get( '/get_messages', function(request, response){
+	var parsed_url = require('url').parse(request.url, true);
+	var query = parsed_url.query;
+	console.log('Received lat: ' + query.lat + ', lon: ' + query.lon);
+	//response.send('Received lat: ' + query.lat + ', lon: ' + query.lon);
+	var test_data = [
+		{
+			location : {
+				type : "Point",
+				coordinates : [45, 45]
+			},
+			board : ["test message"],
+			board_id : "696969696969"
+		}
+	];
+	response.send(test_data);
+});
+
+// POST: /send_message
+app.post( '/send_message', function(request, response){
+});
+
+var server = app.listen(8888, function() {
+	var host = server.address().address;
+	var port = server.address().port;
+	
+	console.log("Listening at http://%s:%s", host, port);
+});
