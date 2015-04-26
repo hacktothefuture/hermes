8857b7690e37827c41c5f65135f1769673e46ad6
Logan J Daniels <danie855@umn.edu>
2015-04-26 04:51:31 -0500

    Implement message view list adapter

--- android/app/src/main/java/com/hacktothefuture/hermes/MessageViewActivity.java
+++ android/app/src/main/java/com/hacktothefuture/hermes/MessageViewActivity.java
@@ -3,13 +3,29 @@ package com.hacktothefuture.hermes;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.squareup.otto.Subscribe;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MessageViewActivity extends ActionBarActivity {
+    private static final String TAG = "MessageViewActivity";
 
     String m_boardId;
+    List<Board> m_boards;
+    private ListView m_listView;
+    private List<String> m_messages;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +36,17 @@ public class MessageViewActivity extends ActionBarActivity {
         m_boardId = i.getStringExtra(LocationCheckService.EXTRA_BOARD_ID);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        LocationBus.getInstance().register(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        LocationBus.getInstance().unregister(this);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -41,5 +68,32 @@ public class MessageViewActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Subscribe
+    public void onBoardUpdate(ArrayList<Board> boards) {
+        if (m_boards != null) return;
+        m_boards = boards;
+        if (boards == null) {
+            Log.e(TAG, "Boards is null");
+        }
+        m_listView = (ListView) findViewById(R.id.message_view_listview);
+        for (Board board : boards) {
+            if (board.get_id().equals(m_boardId)) {
+                m_messages = board.getMessages();
+            }
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter(this, R.layout.message_list_item, m_messages) {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                LayoutInflater inflater = (LayoutInflater) getLayoutInflater();
+                View rowView = inflater.inflate(R.layout.message_list_item, parent, false);
+                TextView textView = (TextView) rowView.findViewById(R.id.message_list_textview);
+                textView.setText((String)getItem(position));
+                return rowView;
+            }
+        };
+        m_listView.setAdapter(adapter);
+
     }
 }
